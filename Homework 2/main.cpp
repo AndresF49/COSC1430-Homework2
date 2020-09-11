2fc041a6d5e4a3c01ed29c1d0cf6878629e1a620
Andres Flores <andres06037@gmail.com>
2020-09-11 13:51:39 -0500

    Initial Commit

---
+++ Homework 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework 2
+//
+//  Created by Andres Flores on 9/11/20.
+//  Copyright © 2020 Andres Flores. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
